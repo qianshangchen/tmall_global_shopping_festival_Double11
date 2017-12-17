0629638b30aaa0b0fd2142c2769da73e395d5ab9
qianshangchen <31749021+qianshangchen@users.noreply.github.com>
2017-12-17 02:32:25 -0500

    Update test1.js

--- test1.js
+++ test1.js
@@ -3,24 +3,32 @@ var svg = d3.select("svg"),
     width = +svg.attr("width") - margin.left - margin.right,
     height = +svg.attr("height") - margin.top - margin.bottom,
     g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
 var x0 = d3.scaleBand()
     .rangeRound([0, width])
     .paddingInner(0.1);
+
 var x1 = d3.scaleBand()
     .padding(0.05);
+
 var y = d3.scaleLinear()
     .rangeRound([height, 0]);
+
 var z = d3.scaleOrdinal()
     .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
-d3.csv("dataset1.csv", function(d, i, columns) {
+
+d3.csv("data.csv", function(d, i, columns) {
   for (var i = 1, n = columns.length; i < n; ++i) d[columns[i]] = +d[columns[i]];
   return d;
 }, function(error, data) {
   if (error) throw error;
+
   var keys = data.columns.slice(1);
+
   x0.domain(data.map(function(d) { return d.Festival; }));
   x1.domain(keys).rangeRound([0, x0.bandwidth()]);
   y.domain([0, d3.max(data, function(d) { return d3.max(keys, function(key) { return d[key]; }); })]).nice();
+
   g.append("g")
     .selectAll("g")
     .data(data)
@@ -34,10 +42,12 @@ d3.csv("dataset1.csv", function(d, i, columns) {
       .attr("width", x1.bandwidth())
       .attr("height", function(d) { return height - y(d.value); })
       .attr("fill", function(d) { return z(d.key); });
+
   g.append("g")
       .attr("class", "axis")
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(x0));
+
   g.append("g")
       .attr("class", "axis")
       .call(d3.axisLeft(y).ticks(null, "s"))
@@ -49,6 +59,7 @@ d3.csv("dataset1.csv", function(d, i, columns) {
       .attr("font-weight", "bold")
       .attr("text-anchor", "start")
       .text("Million Dollars");
+
   var legend = g.append("g")
       .attr("font-family", "sans-serif")
       .attr("font-size", 10)
@@ -57,14 +68,18 @@ d3.csv("dataset1.csv", function(d, i, columns) {
     .data(keys.slice().reverse())
     .enter().append("g")
       .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
   legend.append("rect")
       .attr("x", width - 19)
       .attr("width", 19)
       .attr("height", 19)
       .attr("fill", z);
+
   legend.append("text")
       .attr("x", width - 24)
       .attr("y", 9.5)
       .attr("dy", "0.32em")
       .text(function(d) { return d; });
-    });
+});
+
+</script>
